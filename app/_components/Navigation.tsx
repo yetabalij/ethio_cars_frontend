323dc579846ce619f52efc038881af54aa6a1db2
Ermias Tsegu <yetabalij@gmail.com>
2025-01-13 14:28:58 +0300

    navigation menu completed.

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,14 +1,32 @@
 import Image from "next/image";
 
 import Logo from "./../../public/logo.png";
+import Link from "next/link";
 
-const navigation = () => {
+const navigation = ({ className }: { className?: string }) => {
   return (
     <header className="bg-[#F4F7FB]">
-      <div className="flex justify-between content-center justify-items-center py-4 mx-20">
-        <Image src={Logo} width={80} alt="Logo" />
-        <div>navigation</div>
-        <div>navigation</div>
+      <div className="flex justify-between items-center py-4 mx-20">
+        <Image src={Logo} width={100} alt="Logo" />
+
+        <div className="text-[#07234D] text-2xl">
+          <Link href="/" className="px-10">
+            Home
+          </Link>
+          <Link href="/cars" className="px-10">
+            Cars
+          </Link>
+          <Link href="/about" className="px-10">
+            About
+          </Link>
+          <Link href="/contact" className="px-10">
+            Contact
+          </Link>
+        </div>
+
+        <div className={`text-[#07234D] text-lg ${className}`}>
+          <Link href="/login">Login</Link>
+        </div>
       </div>
     </header>
   );
