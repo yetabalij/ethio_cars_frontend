74b523df93c3c13c355f6fa21af68eba69d572bc
Ermias Tsegu <yetabalij@gmail.com>
2025-01-17 17:44:07 +0300

    admin login page is completed.

--- app/(adminPages)/admin/login/page.tsx
+++ app/(adminPages)/admin/login/page.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(4, { message: "Username must be at least 4 characters" }),
+  password: z
+    .string()
+    .min(4, { message: "Password must be at least 4 characters" }),
+});
 
 const AdminLogin = () => {
-  return <div>AdminLogin</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <section className="w-[25%] mx-auto mt-10">
+        <h1 className="text-center">Admin Login</h1>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            method="POST"
+            className="space-y-8"
+          >
+            {/* Username Field */}
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            {/* Password Field */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full bg-[#B52F31] text-white" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </section>
+    </div>
+  );
 };
 
 export default AdminLogin;
